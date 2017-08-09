5120343bdb9c1e707db8228fc505eb67ee519fbb
Kyrilos Vourlakidis <kyrilos.vourlakidis@ibm.com>
2017-08-09 14:01:26 +0100

    Added some Project Euler problems.

---
+++ ProjectEuler/Problem004.java
@@ -0,0 +1,36 @@
+public class Problem004 {
+	public static void main(String[] args) {
+		Kirill.main();
+	}
+}
+
+class Kirill {
+	static void main() {
+		int largestPal = 0;
+		for (int i = 100; i < 999; i++) {
+			for (int j = 100; j < 999; j++) {
+				int ij = i*j;
+				if (isPalindrome(ij) && ij > largestPal) {
+					largestPal = ij;
+					System.out.println(i + " * " + j + " = " + largestPal);
+				}
+			}
+		}
+	}
+
+	static boolean isPalindrome(int x) {
+		String fwdStr = Integer.toString(x);
+		String revStr = reverse(fwdStr);
+		if (fwdStr.equals(revStr)) return true;
+		return false;
+	}
+
+	static String reverse(String s) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = (s.length() - 1); i >= 0; i--) {
+			sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
+
+}
