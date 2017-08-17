de684a7a56c25f5c2742fa57ddf572d254945bd5
Kyrilos Vourlakidis <kyrilos.vourlakidis@ibm.com>
2017-08-17 09:03:40 +0100

    Ch 2 stuff.

---
+++ SelikoffBoyarsky/chapter2/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+	public static void main(String[] args) {
+		int x = +3;
+		int y = -x++;
+		System.out.println(y);
+		System.out.println(x);
+//		boolean z = (-y == --x) ;
+//		if (z) x = --y;
+//		System.out.println(x);
+
+//		x = 1;
+//		y = (x++);
+//		System.out.println(y);
+
+//		for (int i=0;++i==0;i--) System.out.println("So long, suckers!");  
+	}
+}
