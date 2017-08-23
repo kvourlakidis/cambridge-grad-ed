c10692a34d8213fba4c86e9fce9339fb00366685
Kyrilos Vourlakidis <kyrilos.vourlakidis@ibm.com>
2017-08-23 21:56:38 +0100

    Euler problem 14.

--- ProjectEuler/Problem014.java
+++ ProjectEuler/Problem014.java
@@ -7,21 +7,21 @@
 public class Problem014 {
 	public static void main(String[] args) {
 		int maxSeq = 0;
-		for (int i=1;i<=10_000;i++) {
+		for (int i=1;i<=1_000_000;i++) {
 			int seq = collatz(i,1,false);
 			if (seq > maxSeq) {
 				maxSeq = seq;
 				System.out.print(i);
-				collatz(i,1,true);
-				System.out.print(" (" + seq + ")");
-				System.out.print("\n\n\n");
+				// collatz(i,1,true);
+				System.out.print(" (" + seq + ")\n");
+				// System.out.print("\n\n\n");
 			}
 		}
 	}
 
-	static int collatz(int n, int seq, boolean print) {
+	static int collatz(long n, int seq, boolean print) {
 		if (n == 1) return seq;
-		int next;
+		long next;
 		if (n % 2 == 0) next = n / 2;
 		else next = 3 * n + 1;
 		if (print) System.out.print(" - > " + next);
